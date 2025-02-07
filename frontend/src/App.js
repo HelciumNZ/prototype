675e5e375d1cdd9c9bc25ee3edbb116b9e5291dc
Helcio M. Pimentel <helcio.pimentel.nz@gmail.com>
2025-02-07 16:04:10 +1300

    fix. testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,42 @@
 // src/App.js
 import React, { useState, useEffect } from "react";
-import ItemForm from "./components/ItemForm";
-import ItemList from "./components/ItemList";
-import ItemController from "./controllers/itemController";
-import ItemModel from "./models/itemModel";
+// import ItemForm from "./components/ItemForm";
+// import ItemList from "./components/ItemList";
+// import ItemController from "./controllers/itemController";
+// import ItemModel from "./models/itemModel";
 
 function App() {
-  const [items, setItems] = useState([]);
-  const [form, setForm] = useState(new ItemModel());
-  const [editingId, setEditingId] = useState(null);
+  // const [items, setItems] = useState([]);
+  // const [form, setForm] = useState(new ItemModel());
+  // const [editingId, setEditingId] = useState(null);
 
   // useEffect(() => {
   //   ItemController.fetchItems(setItems);
   // }, []);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    await ItemController.saveItem(
-      form, 
-      editingId, 
-      () => ItemController.fetchItems(setItems), 
-      setEditingId, 
-      () => setForm(new ItemModel())
-    );
-  };
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   await ItemController.saveItem(
+  //     form, 
+  //     editingId, 
+  //     () => ItemController.fetchItems(setItems), 
+  //     setEditingId, 
+  //     () => setForm(new ItemModel())
+  //   );
+  // };
 
-  const handleEdit = (item) => {
-    setForm(new ItemModel(
-      item.name, 
-      item.description, 
-      item.price));
-    setEditingId(item._id);
-  };
+  // const handleEdit = (item) => {
+  //   setForm(new ItemModel(
+  //     item.name, 
+  //     item.description, 
+  //     item.price));
+  //   setEditingId(item._id);
+  // };
 
-  const handleDelete = async (id) => {
-    await ItemController.deleteItem(
-      id, () => ItemController.fetchItems(setItems));
-  };
+  // const handleDelete = async (id) => {
+  //   await ItemController.deleteItem(
+  //     id, () => ItemController.fetchItems(setItems));
+  // };
 
   return (
     <div>
